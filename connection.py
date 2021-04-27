8b46454d62394a95776ec1284f578959ddb4d40e
yanoriflores9778 <floresyanori16@gmail.com>
2021-04-27 16:55:23 -0600

    Second commit

---
+++ connection.py
@@ -0,0 +1,10 @@
+import mysql.connector
+connection = mysql.connector.connect(host="localhost", user="root", passwd="")
+
+cursor = connection.cursor()
+cursor.execute("show databases")
+
+for base in cursor:
+    print(base)
+
+connection.close()
